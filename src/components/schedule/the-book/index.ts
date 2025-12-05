e1a1a21b7a77aceb9977e2a0487c37c165ed436a
Port-Arthur-Police-Department <ryan.byers@portarthurtx.gov>
2025-12-05 08:47:01 -0600

    Update index.ts

--- src/components/schedule/the-book/index.ts
+++ src/components/schedule/the-book/index.ts
@@ -1,9 +1,22 @@
+// Main exports
+export { default as TheBook } from './TheBook';
 export { default as TheBookMobile } from './TheBookMobile';
-export { default as ForceListViewMobile } from './ForceListViewMobile';
-export { default as VacationListViewMobile } from './VacationListViewMobile';
-export { default as BeatPreferencesViewMobile } from './BeatPreferencesViewMobile';
-export { default as WeeklyViewMobile } from './WeeklyViewMobile';
-export { default as MonthlyViewMobile } from './MonthlyViewMobile';
-export { PTODialogMobile } from './PTODialogMobile';
+export { default as ResponsiveTheBook } from './ResponsiveTheBook';
+
+// Desktop view components
+export { WeeklyView } from './WeeklyView';
+export { MonthlyView } from './MonthlyView';
+export { ForceListView } from './ForceListView';
+export { VacationListView } from './VacationListView';
+export { BeatPreferencesView } from './BeatPreferencesView';
+
+// Mobile view components
+export { WeeklyViewMobile } from './WeeklyViewMobile';
+export { MonthlyViewMobile } from './MonthlyViewMobile';
+export { ForceListViewMobile } from './ForceListViewMobile';
+export { VacationListViewMobile } from './VacationListViewMobile';
+export { BeatPreferencesViewMobile } from './BeatPreferencesViewMobile';
+
+// Shared
 export * from './types';
 export * from './utils';

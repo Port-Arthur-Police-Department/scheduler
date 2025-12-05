c9155196a49110e51e60042ef39964530c696c87
Port-Arthur-Police-Department <ryan.byers@portarthurtx.gov>
2025-12-05 08:29:35 -0600

    Create use-media-query.ts

---
+++ src/hooks/use-media-query.ts
@@ -0,0 +1,7 @@
+import { useState, useEffect } from 'react';
+
+export function useMediaQuery(query: string): boolean {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const media = window.match

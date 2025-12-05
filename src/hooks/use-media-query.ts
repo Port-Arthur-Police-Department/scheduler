0210b112777abe19b27b9c5f274c771ed03ff677
Port-Arthur-Police-Department <ryan.byers@portarthurtx.gov>
2025-12-05 08:29:58 -0600

    Update use-media-query.ts

--- src/hooks/use-media-query.ts
+++ src/hooks/use-media-query.ts
@@ -4,4 +4,14 @@ export function useMediaQuery(query: string): boolean {
   const [matches, setMatches] = useState(false);
 
   useEffect(() => {
-    const media = window.match
+    const media = window.matchMedia(query);
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+    const listener = () => setMatches(media.matches);
+    window.addEventListener('resize', listener);
+    return () => window.removeEventListener('resize', listener);
+  }, [matches, query]);
+
+  return matches;
+}

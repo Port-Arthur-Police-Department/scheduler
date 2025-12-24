4d91c75bf277cb47263ae20200c36b02ef709fd3
Port-Arthur-Police-Department <ryan.byers@portarthurtx.gov>
2025-12-24 13:16:43 -0600

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,5 @@
+// This file is intentionally minimal - OneSignal will override it
+console.log('Police Department Scheduler Service Worker Loading...');
+
+// Import the OneSignal SDK Worker
+importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');

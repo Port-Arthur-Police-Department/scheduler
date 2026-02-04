0ebec803489f91c8531f3a5767910e8cabc5b9be
Port-Arthur-Police-Department <ryan.byers@portarthurtx.gov>
2026-02-04 13:55:37 -0600

    Create scheduleDataUtils.ts to export schedule functions

---
+++ src/utils/scheduleDataUtils.ts
@@ -0,0 +1,3 @@
+// src/utils/scheduleDataUtils.ts
+export { getScheduleData } from '../components/schedule/DailyScheduleView';
+export { isBirthdayToday, isAnniversaryToday, calculateYearsOfService } from '../components/schedule/DailyScheduleView';
